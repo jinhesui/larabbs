e1a41a4cd31bdc170479106cdb1e49fb6e7de990
jinhesui <jinhesui@163.com>
2022-05-25 16:11:11 +0800

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
   <title>@yield('title', 'LaraBBS') - 星的天空</title>
   <!-- Styles -->
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  @yield('styles')
 </head>
 <body>
   <div id="app" class="{{ route_class() }}-page">
@@ -21,5 +22,6 @@
   </div>
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
+  @yield('scripts')
 </body>
 </html>

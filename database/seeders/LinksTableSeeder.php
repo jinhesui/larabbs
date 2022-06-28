aa3fea84f369a099ba9333f86e1f1a25f3b740a9
jinhesui <jinhesui@163.com>
2022-06-28 16:48:06 +0800

    侧边栏链接

---
+++ database/seeders/LinksTableSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Link;
+
+class LinksTableSeeder extends Seeder
+{
+    public function run()
+    {
+        Link::factory()->times(6)->create();
+    }
+}
